ee1f5fa776210a7554df4bc0b4acba815312a929
Mohamed Habas <habas.mohamed@gmail.com>
2020-11-28 03:48:16 +0100

    fix install

--- build_repo.sh
+++ build_repo.sh
@@ -1,10 +1,11 @@
 # Make executable with chmod +x <<filename.sh>>
 
-CURRENTDIR=${pwd}
+source env.sh
+source install.sh
 
 # step 1: name of the remote repo. Enter a SINGLE WORD ..or...separate with hyphens
-echo "What name do you want to give your remote repo?"
-read REPO_NAME
+echo "**Working on creating the repo**"
+
 
 echo "Enter a repo description: "
 read DESCRIPTION
@@ -13,9 +14,6 @@ read DESCRIPTION
 # step 2:  the local project folder path
 echo "what is the absolute path to your local project directory?"
 read PROJECT_PATH
-
-echo "What is your github username?"
-read USERNAME
 
 # step 3 : go to path 
 cd "$PROJECT_PATH"
@@ -29,14 +27,14 @@ git commit -m 'initial commit -setup with .sh script'
 
 
 # step 5 use github API to log the user in
-curl -u ${USERNAME} https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}"
+curl -u ${User_name} https://api.github.com/user/repos -d "{\"name\": \"${repo_name}\", \"description\": \"${DESCRIPTION}\"}"
 
 #  step 6 add the remote github repo to local repo and push
-git remote add origin https://github.com/${USERNAME}/${REPO_NAME}.git
+git remote add origin https://github.com/${User_name}/${repo_name}.git
 git push --set-upstream origin master
 
 # step 7 change to your project's root directory.
 cd "$PROJECT_PATH"
 
-echo "Done. Go to https://github.com/$USERNAME/$REPO_NAME to see." 
+echo "Done. Go to https://github.com/$User_name/$repo_name to see." 
 echo " *** You're now in your project root. ***"
